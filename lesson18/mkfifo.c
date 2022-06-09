3b603386bf835c73f73f12c22e0481fd74c7ceb1
ihbal61 <yikebaer.125.1@qq.com>
2022-06-09 22:08:57 +0800

    updates

--- lesson18/mkfifo.c
+++ lesson18/mkfifo.c
@@ -10,7 +10,7 @@
             - pathname: 管道名称的路径
             - mode: 文件的权限 和 open 的 mode 是一样的
                     是一个八进制的数
-        返回值：成功返回0，失败返回-1，并设置错误号
+        返回值：成功返回0，失败返回-1，并设置错误号errno
 
 */
 
@@ -25,18 +25,16 @@ int main() {
 
     // 判断文件是否存在
     int ret = access("fifo1", F_OK);
-    if(ret == -1) {
+    if(ret == -1){
         printf("管道不存在，创建管道\n");
-        
+
         ret = mkfifo("fifo1", 0664);
 
-        if(ret == -1) {
+        if(ret == -1){
             perror("mkfifo");
             exit(0);
-        }       
-
+        }
     }
-
     
 
     return 0;
